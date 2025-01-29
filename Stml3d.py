1520c8d66925d89cfcc0dbbb13495866d25a7a3b
Vic Vic <104849763+Victorious97@users.noreply.github.com>
2025-01-29 15:00:41 +0200

    Update Stml3d.py

--- Stml3d.py
+++ Stml3d.py
@@ -10,7 +10,7 @@ import pandas as pd
 import numpy as np
 
 # Set page title
-st.set_page_config(page_title="Researcher Profile and STEM Data Explorer", layout="wide")
+st.set_page_config(page_title="Researcher Profile", layout="wide")
 
 # Sidebar Menu
 st.sidebar.title("Navigation")
